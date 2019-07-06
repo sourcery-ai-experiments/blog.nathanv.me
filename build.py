a53f7d395f3159bf5bb926fd439405c8790dab34
Nathan Vaughn <nvaughn51@gmail.com>
2019-07-06 12:27:03 -0500

    Fix build script

--- build.py
+++ build.py
@@ -1,20 +1,33 @@
+import glob
 import os
 import shutil
 import subprocess
 import sys
+import time
 
 
 def main():
     os.chdir("public")
     subprocess.call(["git", "submodule", "update"])
+    time.sleep(1)
     subprocess.call(["git", "reset", "--hard", "master"])
+    time.sleep(1)
     os.chdir("..")
-    shutil.rmtree("public")
+    delete_folder_contents("public")
+    time.sleep(1)
     subprocess.call(["hugo"])
 
     with open("public/CNAME", "w") as f:
         f.write("blog.nathanv.me")
 
+def delete_folder_contents(folder):
+    pattern = folder + '/*'
+    r = glob.glob(pattern)
+    for i in r:
+        if os.path.isfile(i):
+            os.remove(i)
+        elif os.path.isdir(i):
+            shutil.rmtree(i)
 
 if __name__ == "__main__":
     main()
