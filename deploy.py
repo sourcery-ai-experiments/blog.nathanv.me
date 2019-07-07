b06cef4f5b5f85c2a0a6c7efa5561e13fcbdeff3
Nathan Vaughn <nvaughn51@gmail.com>
2019-07-07 12:46:09 -0500

    RSS fixes

--- deploy.py
+++ deploy.py
@@ -10,5 +10,10 @@ def main():
     subprocess.call(["git", "commit", "-m", sys.argv[1]])
     subprocess.call(["git", "push", "-f", "origin", "master"])
 
+    os.chdir("..")
+    subprocess.call(["git", "add", "."])
+    subprocess.call(["git", "commit", "-m", sys.argv[1]])
+    subprocess.call(["git", "push", "-f", "origin", "master"])
+
 if __name__ == "__main__":
     main()
